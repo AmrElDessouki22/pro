8ae8c676b4cda475166f5f4250b52ea7463460e5
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:30:37 +0300

    Add src/app/loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const GlobalLoading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default GlobalLoading;
