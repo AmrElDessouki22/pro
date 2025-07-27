dceee2c05d267319b3e247c358258fb2b6c42677
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-27 14:30:39 +0300

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import NotesList from '@/components/features/NotesList';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">My Notes</h1>
+      <NotesList />
+    </div>
+  );
+};
+
+export default HomePage;
